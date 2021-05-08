649e68ab17d6882cfa881a1ba992dfc77efb993a
“ouissalsahnoun” <ajuraiss@gmail.com>
2021-05-09 01:34:22 +0300

    work in progess

--- ouissal-ines-recursion.js
+++ ouissal-ines-recursion.js
@@ -1,113 +1,191 @@
-// Wissal-Ines 
-//04/05/2021
-//recursion 
+
 //Q1
+//Answer 
 function sum(number){
-if ( number==-5){
-return 0}
+if ( number==0){
+
+return 0
+}
+
 return number+sum(number-1)
 }
-undefined
-sum(3)
-6
-sum(4)
-10
+
+
+
+
+
+
 //Q2
+
+// answer 
+
 function factorial(number){
-    if (number===0){
-        return 1}
-    return number*factorial(number-1)}
-undefined
-factorial(3)
-6
-factorial(4)
-24
+	if (number===0){
+  return 1}
+
+ return number * factorial(number-1)
+ }
+
+
 //Q3
+// answer 
+
 function repeatString(string,number){
     if (number===0){
-        return ""}
-    return   string + repeatString(string,number-1)  }
-undefined
-repeatString('dog',0)
-""
-repeatString('dog',2)
-"dogdog"
-//Q4
+    	return ""
+    }
+
+return string + repeatString(string,(number-1))
+}
+
+
+// Q4
+// answer 
+
 function fibonacci(number){
-var number1=0;
-var number2=1;
-var sum=0;
-    if (number===1){
-        return 1}
-   sum=number1+number2;
-  number2=number1;
-  number1=sum;
-    return  }
-1+1=2
-2+1=3
-3+2=5
-5+3=8
-8+5=13
-number1+number2=number3
-number2=number1
-number1=number3
-function fibonacci(number){
-var number1=0;
-var number2=1;
-var sum=0
-for (i=0;i<=number;i++){
-  sum=number1+number2;
-  number2=number1;
-  number1=sum;}
-return sum }
-//Q5
-function multiplyBy10(firstNumber, secondNumber) {
-function help(secondNumber){
-  if (secondNumber===0){
-   return 1}
-   return 10*help(secondNumber-1)
-  }
-return firstNumber * help(secondNumber)}
-undefined
-multiplyBy10(4,3)
-4000
-multiplyBy10(5,2)
-500
-//Q6
-function sumBetween(start, end) {
-  if (start>end){
-   var a=start;
-   start=end;
-   end=a } 
-  if (start===end){
-      return end}
-    return start+sumBetween(start+1,end)}
-undefined
-sumBetween(2,7)
-27
-//Q7
+ 
+
+
+
+}
+
+
+
+// Q5 
+// answer 
+
+function multiplyBy10(number1,number2){
+
+	function help(number2){
+
+
+	if ((number2)===0){
+		return 1
+	}
+	return 10 * help((number2)-1)
+   }
+
+	
+   return number1* help(number2)
+	}
+
+
+
+// Q6
+// Answer 
+function sumBetween(start,end){
+if (start>end){
+	var a= start;
+	start=end;
+	end=a
+}
+if (start===end){
+	return end }
+	return start + sumbetween(start+1,end)
+}
+
+
+
+
+
+// Q7
+// answer 
+
+
+function INC(x){
+
+	return x+1
+}
+
+
+function DEC(x){
+
+	return x-1
+}
+
+
+
 function add(number1,number2){
-    if (number1===0){
-        return number2}
-    return add(dec(number1),inc(number2))}
-undefined
-function inc(x) {
-return x + 1
-}
-function dec(x) {
-return x - 1
-}
-undefined
-add(1,2)
-3
+
+	if (number1===0){ 
+		return number2}
+
+    return add(DEC(number1),INC(number2))
+	}
+
+
 //Q8
-//Q9
-function multiply(number1, number2){
-  if (number2===0){
-   return 0}
-return number1+multiply(number1,number2-1)}
-undefined
-multiply(3,2)
-6
-multiply(5,5)
-25
+// answer 
+function dec(x){
+
+
+	return x-2
+}
+
+function isEven(number){
+
+	if (number===1)
+		{return false }
+
+      if (number===0)
+      	{ return true }
+
+return isEven(dec(number))
+
+}
+
+
+// Q9
+// answer 
+function multiply(number1,number2){
+if (number2===0) {
+
+	return 0
+
+}
+return number1+multiply(number1,((number2)-1))
+
+
+
+}
+
+
+
+// Q10 
+// answer 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
